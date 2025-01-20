8bf92bb00c45511483ee5038b94bb09609d81bda
Djello Nicolas <djellodesignerweb@gmail.com>
2025-01-20 14:26:13 -0200

    Bussiness
    Coding--------------------------

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
-   $('#mobile-btn').on('click', function() {
-      $('#mobile-menu'). toggleCLass('.active');
-   });
+
+//Aqui adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburguer.
+mobile_btn.addEventListener("click", function() {
+   mobile_menu.classList.add("flex");
+   //Criamos uma função que a ouvir o click do Mouse, ele adicione uma Class, que está fazendo nossa caixa (Div) aparecer, ou seja, ficar visível.
+});
+
+//Criamos uma função que a ouvir o click do Mouse, ele REMOVA uma Class, que está fazendo nossa caixa (Div) desaparecer, ou seja, ficar invisível.
+closeButton.addEventListener("click", function() {
+   mobile_menu.classList.remove("flex");
 });
